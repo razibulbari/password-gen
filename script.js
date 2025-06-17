db27abb498245787833b30bbb6a04538c10a071d
razibulbari <99231895+razibulbari@users.noreply.github.com>
2025-06-17 16:20:54 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-const passwordBox = document.getElementById("password");
-const length = 12;
-const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:,.<>?";
-function generatePassword() {
-    let password = "";
-    for (let i = 0, n = charset.length; i < length; ++i) {
-        password += charset.charAt(Math.floor(Math.random() * n));
-    }
-    passwordBox.value = password;
-}
+it's empty hehe
