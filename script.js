aae757105a088a6c42fde761d412cad0738dfa72
razibulbari <99231895+razibulbari@users.noreply.github.com>
2025-06-17 16:20:03 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:,.<>?";
+function generatePassword() {
+    let password = "";
+    for (let i = 0, n = charset.length; i < length; ++i) {
+        password += charset.charAt(Math.floor(Math.random() * n));
+    }
+    passwordBox.value = password;
+}
